d086d6ff9e2def1538af2db222021282d9342d63
Иван Соколов <vano95.cokol@gmail.com>
2023-09-19 13:19:56 +0600

    task 3 done

--- HW1/Ex3.py
+++ HW1/Ex3.py
@@ -3,3 +3,18 @@
 # Для генерации случайного числа используйте код:
 # from random import randint
 # num = randint(LOWER_LIMIT, UPPER_LIMIT)
+
+from random import randint
+num = randint(0, 1000)
+
+while True:
+  number = int(input("Введите число от 0 до 1000: "))
+  if 0 <= number <= 1000:
+    if number > num: 
+      print("Mеньше")
+    elif number < num:
+      print("Больше")
+    else: 
+      print("Вы угадали загаданное число!")
+      break
+  else: print("Введите допустимое число!")
