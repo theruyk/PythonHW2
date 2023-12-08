0c578b5b42c4cd8f3d40ef57a91988870fabd765
Иван Соколов <vano95.cokol@gmail.com>
2023-12-08 18:37:29 +0600

    done task1

---
+++ HW9/Task1.py
@@ -0,0 +1,12 @@
+import subprocess
+from typing import Tuple
+def testing_text(command: str, text: str) -> bool:
+    try:
+        result = subprocess.run(command, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout
+        return text in output
+    except subprocess.CalledProcessError:
+        return False
+
+#print(testing_text('echo "Hello, world!"', 'Hello, world!')) #True
+#print(testing_text( 'not_a_comand', 'Hello, world!'))# False
